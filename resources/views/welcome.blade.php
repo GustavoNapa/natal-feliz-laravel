850ba8e6c787510f26551be98cce072600467518
Gustavo Souza <gustavosouzacco@gmail.com>
2023-11-30 00:08:29 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Natal Feliz Laravel</title>
+
+    <style>
+        body{
+            margin: 0;
+        }
+        #header{
+            height: 100vh;
+            width: 100vw;
+
+            background: rgb(4,90,45);
+            background: linear-gradient(152deg, rgba(4,90,45,1) 0%, rgba(21,50,32,1) 100%);
+        }
+    </style>
+</head>
+<body>
+    <header id="header" class="w-100 h-100 container">
+        <div class="logo">
+            <img src="https://growthsistemas.com.br/images/LOGOTIPOCLARO.png" alt="Growth Sites e Sistemas" width="160px">
+
+            <h3>25 de Dezembro de 2023</h3>
+            <h1>Feliz Natal</h1>
+            <h2>Promoção especial</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eaque ratione commodi a nihil reprehenderit hic culpa, aliquid error sint? At nisi id nobis itaque consectetur eligendi, pariatur numquam. Accusamus.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae nemo, illo optio eius voluptatibus vel quis ipsa, eligendi iusto quia blanditiis quidem aut natus adipisci expedita fuga suscipit dolor! Debitis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam debitis facere totam dolores atque reprehenderit provident impedit modi expedita, omnis, fuga quia porro explicabo molestias quos quam incidunt tenetur! Totam. 
+            </p>
+        </div>
+    </header>
+</body>
+</html>
